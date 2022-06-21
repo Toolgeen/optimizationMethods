7e2efcb3a0c19c468e13064a9bf5370992ff2c95
Tol <htemohpab@gmail.com>
2022-06-21 15:26:00 +0600

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,22 @@
+fun main() {
+    println("Hello World!")
+
+    val rows = 4
+    val cols = 4
+    val x = arrayOf(
+        intArrayOf(800, 100, 900, 300),
+        intArrayOf(400, 600, 200, 1200),
+        intArrayOf(700, 500, 800, 900),
+        intArrayOf(400, 900, 0, 500)
+    )
+
+    val base = arrayOf(
+        intArrayOf(0, 0, 0, 0),
+        intArrayOf(0, 0, 0, 0),
+        intArrayOf(0, 0, 0, 0),
+        intArrayOf(0, 0, 0, 0)
+    )
+    
+    val a = arrayOf(110, 190, 90, 70)
+    val b = arrayOf(100, 60, 170, 130)
+}
