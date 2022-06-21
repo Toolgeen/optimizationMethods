d0d901c9e6b36cf859759b82aae1f9254224f9ad
Tol <htemohpab@gmail.com>
2022-06-21 16:08:24 +0600

    least element found

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,4 @@
 fun main() {
-    println("Hello World!")
 
     val rows = 4
     val cols = 4
@@ -7,7 +6,7 @@ fun main() {
         intArrayOf(800, 100, 900, 300),
         intArrayOf(400, 600, 200, 1200),
         intArrayOf(700, 500, 800, 900),
-        intArrayOf(400, 900, 0, 500)
+        intArrayOf(400, 900, 23, 500)
     )
 
     val base = arrayOf(
@@ -16,7 +15,30 @@ fun main() {
         intArrayOf(0, 0, 0, 0),
         intArrayOf(0, 0, 0, 0)
     )
-    
+
     val a = arrayOf(110, 190, 90, 70)
     val b = arrayOf(100, 60, 170, 130)
+
+    var count = 0
+
+    val answer = findMinElementFromMatrix(x)
+
+    println("${answer[0]}, ${answer[1]}, ${answer[2]}")
+
+}
+
+fun findMinElementFromMatrix(matrix: Array<IntArray>) : Array<Int> {
+    var leastCostRow = 0
+    var leastCostCol = 0
+    var leastCost = matrix[leastCostRow][leastCostCol]
+    for (row in matrix.indices) {
+        for (col in matrix[row].indices) {
+            if (leastCost > matrix[row][col]) {
+                leastCostRow = row
+                leastCostCol = col
+                leastCost = matrix[row][col]
+            }
+        }
+    }
+    return arrayOf(leastCost, leastCostRow, leastCostCol)
 }
