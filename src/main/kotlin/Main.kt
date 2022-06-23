e9dad932babc7d1e4b3074ac0fc663a5dcbf935f
Tol <htemohpab@gmail.com>
2022-06-23 13:07:04 +0600

    refactor to double

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,4 @@
-private const val BASE_VALUE = 0
+private const val BASE_VALUE = 0.0
 
 fun main() {
 
@@ -9,6 +9,8 @@ fun main() {
     val disabledCols = BooleanArray(cols) {true}
 
     var x = insertMatrix(rows, cols)
+    val a = insertAMatrix(rows)
+    val b = insertBMatrix(cols)
 
 //    var x = arrayOf(
 //        intArrayOf(800, 100, 900, 300),
@@ -17,10 +19,12 @@ fun main() {
 //        intArrayOf(400, 900, 0, 500)
 //    )
 
-    val base = Array(rows){IntArray(cols){BASE_VALUE} }
+//    val a = arrayOf(110, 190, 90, 70)
+//    val b = arrayOf(100, 60, 170, 130)
 
-    val a = arrayOf(110, 190, 90, 70)
-    val b = arrayOf(100, 60, 170, 130)
+    val base = Array(rows){DoubleArray(cols){BASE_VALUE} }
+
+
     var iterations = 0
 
     val maxElement = findMaxElementFromMatrix(x)
@@ -28,8 +32,8 @@ fun main() {
     while ((rows != 1) || (cols != 1)) {
         val findLeastElement = findMinElementFromMatrix(x)
         val leastCost = findLeastElement[0]
-        val leastCostRow = findLeastElement[1]
-        val leastCostCol = findLeastElement[2]
+        val leastCostRow = findLeastElement[1].toInt()
+        val leastCostCol = findLeastElement[2].toInt()
         println("least element = $leastCost, indices $leastCostRow:$leastCostCol")
 
         base[leastCostRow][leastCostCol] = if (a[leastCostRow] > b[leastCostCol]) {
@@ -41,11 +45,11 @@ fun main() {
 
         a[leastCostRow] -= base[leastCostRow][leastCostCol]
         b[leastCostCol] -= base[leastCostRow][leastCostCol]
-        if (a[leastCostRow] == 0) {
+        if (a[leastCostRow] == BASE_VALUE) {
             disabledRows[leastCostRow] = false
             x = disableRow(x,leastCostRow, maxElement)
             rows--
-        } else if (b[leastCostCol] == 0) {
+        } else if (b[leastCostCol] == BASE_VALUE) {
             disabledCols[leastCostCol] = false
             x = disableColumn(x,leastCostCol, maxElement)
             cols--
@@ -83,7 +87,7 @@ fun main() {
     println("SOLVED")
 }
 
-fun disableColumn(matrix: Array<IntArray>, column: Int, maxElement: Int) : Array<IntArray> {
+fun disableColumn(matrix: Array<DoubleArray>, column: Int, maxElement: Double) : Array<DoubleArray> {
     println("disabled column = $column")
     matrix.map {
         it[column] = maxElement
@@ -91,21 +95,21 @@ fun disableColumn(matrix: Array<IntArray>, column: Int, maxElement: Int) : Array
     return matrix
 }
 
-fun disableRow(matrix: Array<IntArray>, disabledRow: Int, maxElement: Int) : Array<IntArray> {
+fun disableRow(matrix: Array<DoubleArray>, disabledRow: Int, maxElement: Double) : Array<DoubleArray> {
     println("disabled row = $disabledRow")
-    val newMatrix = mutableListOf<IntArray>()
+    val newMatrix = mutableListOf<DoubleArray>()
     for (row in matrix.indices) {
         if (row != disabledRow) {
             newMatrix.add(row, matrix[row])
         } else {
-            newMatrix.add(row, IntArray(matrix[row].size){maxElement})
+            newMatrix.add(row, DoubleArray(matrix[row].size){maxElement})
         }
     }
     return newMatrix.toTypedArray()
 }
 
-fun findMaxElementFromMatrix(matrix: Array<IntArray>) : Int {
-    var max = 0
+fun findMaxElementFromMatrix(matrix: Array<DoubleArray>) : Double {
+    var max = BASE_VALUE
     matrix.map {
         it.map { matrixElement ->
             if (max < matrixElement) {
@@ -116,7 +120,7 @@ fun findMaxElementFromMatrix(matrix: Array<IntArray>) : Int {
     return max
 }
 
-fun findMinElementFromMatrix(matrix: Array<IntArray>) : IntArray {
+fun findMinElementFromMatrix(matrix: Array<DoubleArray>) : DoubleArray {
     var leastCostRow = 0
     var leastCostCol = 0
     var leastCost = matrix[leastCostRow][leastCostCol]
@@ -129,10 +133,10 @@ fun findMinElementFromMatrix(matrix: Array<IntArray>) : IntArray {
             }
         }
     }
-    return intArrayOf(leastCost, leastCostRow, leastCostCol)
+    return doubleArrayOf(leastCost, leastCostRow.toDouble(), leastCostCol.toDouble())
 }
 
-fun printMatrix(matrix: Array<IntArray>) {
+fun printMatrix(matrix: Array<DoubleArray>) {
     matrix.map {
         println("[${it.joinToString(", ")}]")
     }
@@ -156,26 +160,55 @@ fun validateInsert(insert : String?) : Boolean {
     return if (insert == null) {
         false
     } else {
-        if (insert.toIntOrNull() is Int) {
-            insert.toInt() >= 0
+        if (insert.toDoubleOrNull() is Double) {
+            insert.toDouble() >= 0
         } else false
     }
 }
 
-fun insertMatrix(rows: Int, cols: Int) : Array<IntArray> {
-    val matrix = mutableListOf<IntArray>()
+fun insertMatrix(rows: Int, cols: Int) : Array<DoubleArray> {
+    val matrix = mutableListOf<DoubleArray>()
     for (i in 0 until rows) {
+        println("INSERTING MATRIX")
         println("Insert #${i+1} row:")
-        val row = mutableListOf<Int>()
+        val row = mutableListOf<Double>()
         for(k in 0 until cols) {
             var value = readLine()
             while (!validateInsert(value)) {
                 println("value row ${i+1}, column ${k+1} not valid, try again")
                 value = readLine()
             }
-            row.add(k,value!!.toInt())
+            row.add(k,value!!.toDouble())
         }
-        matrix.add(row.toIntArray())
+        matrix.add(row.toDoubleArray())
     }
     return matrix.toTypedArray()
+}
+
+fun insertAMatrix(rows: Int) : DoubleArray {
+    println("INSERTING A MATRIX")
+    val matrix = mutableListOf<Double>()
+    for(k in 0 until rows) {
+        var value = readLine()
+        while (!validateInsert(value)) {
+            println("value ${k+1} not valid, try again")
+            value = readLine()
+        }
+        matrix.add(k,value!!.toDouble())
+    }
+    return matrix.toDoubleArray()
+}
+
+fun insertBMatrix(cols: Int) : DoubleArray {
+    println("INSERTING B MATRIX")
+    val matrix = mutableListOf<Double>()
+    for(i in 0 until cols) {
+        var value = readLine()
+        while (!validateInsert(value)) {
+            println("value ${i+1} not valid, try again")
+            value = readLine()
+        }
+        matrix.add(i,value!!.toDouble())
+    }
+    return matrix.toDoubleArray()
 }
